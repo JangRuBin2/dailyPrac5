c3f38956f5842a040d5204cbf0f99ea61564f513
장루빈집 <wkdfnqls2465@gmail.com>
2023-04-24 02:13:40 +0900

    금액 수정

--- jangrubin5.js
+++ jangrubin5.js
@@ -1,14 +1,14 @@
 const basicData = [
   {number : 1, name : "코카콜라", price : 1500},
-  {number : 2, name : "사이다", price : 1500},
-  {number : 3, name : "포카리스웨트", price : 1500},
-  {number : 4, name : "칸쵸", price : 1500},
-  {number : 5, name : "오예스", price : 1500},
-  {number : 6, name : "초코파이", price : 1500},
+  {number : 2, name : "사이다", price : 1200},
+  {number : 3, name : "포카리스웨트", price : 1000},
+  {number : 4, name : "칸쵸", price : 800},
+  {number : 5, name : "오예스", price : 1000},
+  {number : 6, name : "초코파이", price : 1200},
   {number : 7, name : "허니버터칩", price : 1500},
-  {number : 8, name : "새우깡", price : 1500},
-  {number : 9, name : "치즈볼", price : 1500},
-  {number : 10, name : "신라면", price : 1500}
+  {number : 8, name : "새우깡", price : 900},
+  {number : 9, name : "치즈볼", price : 1200},
+  {number : 10, name : "신라면", price : 900}
 ];
 
 const currency = {
@@ -33,13 +33,17 @@ function exampleOne(inputPrice, currency, basicData) {
 let currencyName = Object.values(currency).map(money => money.name)
 let currencyValue = Object.values(currency).map(obj => obj.value)
 
-for(let i=0; i<currencyValue.length; i++) {
-  if(inputPrice === currencyValue[i]) {
-    // console.log(currencyName[i])
-    return console.log(currencyName[i] + "원을 넣었습니다")
-  } 
+// for(let i=0; i<currencyValue.length; i++) {
+//   if(inputPrice === currencyValue[i]) {
+//     // console.log(currencyName[i])
+//     return console.log(currencyName[i] + "원을 넣었습니다")
+//   } 
+// }
+for(let i=0; i<basicData.length; i++) {
+ if(inputPrice >=basicData[0].price) {
+  console.log("gk")
+ }
 }
-
 //  console.log(currencyName)
 //  console.log(currencyValue)
 
